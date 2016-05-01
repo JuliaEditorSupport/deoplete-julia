bb624f44f981354790b62ff569b4697af76dbbd7
Lyndon White <lyndon.white@research.uwa.edu.au>
2016-05-01 11:59:39 +1000

    =more example code

---
+++ samples/subdir/yetanother.jl
@@ -0,0 +1,17 @@
+
+export yet_anthother_a, yet_anthother_b,yet_anthother_c
+
+function yet_anthother_a()
+	1
+end
+
+yet_anthother_b= 5
+
+yet_anthother_c(x)=x
+
+type yet_anthother_d <: AbstractString
+	s
+end
+
+
+
